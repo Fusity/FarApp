45ae758e59112018b554d0f33d0ab64b9fd36f41
Fusity <44893830+Fusity@users.noreply.github.com>
2020-09-28 14:28:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from RPi.GPIO import GPIO
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+from sklearn.linear_model import SGDRegressor
+
+np.random.seed(0)
+x, y = make_regression(n_samples=100, n_features=1, noise=10)
+plt.scatter(x, y)
+
+model = SGDRegressor(max_iter = 1000, eta0 = 0.001)
+model.fit(x, y)
+
+print('Coeff R2 = ', model.score(x, y))
+plt.scatter(x, y)
+plt.plot(x, model.predict(x), c='pink', lw = 3)
